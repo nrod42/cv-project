73185a23680e0e3ad3643a0c902627b46032bc22
Nigel <nigelrod94@gmail.com>
2022-11-17 11:21:32 -0500

    Refactored navbar by making array of tabs and mapping through the tab names and making a nav item for each. Much cleaner code.

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -5,73 +5,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../index.css";
 
 const Navi = () => {
+  const tabs = [
+    {name: 'Home', path: '/cv-project'}, 
+    {name: 'contact', path: '/cv-project/contact-info'}, 
+    {name: 'education', path: '/cv-project/education-history'}, 
+    {name: 'work', path: '/cv-project/work-history'}, 
+    {name: 'projects', path: '/cv-project/projects'}, 
+    {name: 'skills', path: '/cv-project/skills'}, 
+    {name: 'review', path: '/cv-project/review'},
+  ] 
   return (
     <Nav variant="pills" defaultActiveKey="home">
-      <Nav.Item>
-        <Nav.Link as={Link} to={"/cv-project"} eventKey="home" className="text-white">
-          Home
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link
-          as={Link}
-          to={"/cv-project/contact-info"}
-          eventKey="contact"
-          className="text-white"
-        >
-          Contact
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link
-          as={Link}
-          to={"/cv-project/education-history"}
-          eventKey="education"
-          className="text-white"
-        >
-          Education
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link
-          as={Link}
-          to={"/cv-project/work-history"}
-          eventKey="work"
-          className="text-white"
-        >
-          Work
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link
-          as={Link}
-          to={"/cv-project/projects"}
-          eventKey="projects"
-          className="text-white"
-        >
-          Projects
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link
-          as={Link}
-          to={"/cv-project/skills"}
-          eventKey="skills"
-          className="text-white"
-        >
-          Skills
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link
-          as={Link}
-          to={"/cv-project/review"}
-          eventKey="review"
-          className="text-white"
-        >
-          Review
-        </Nav.Link>
-      </Nav.Item>
+      {tabs.map((tab) => {
+        return (
+        <Nav.Item>
+          <Nav.Link as={Link} to={tab.path} eventKey={tab.name} className="text-white">
+            {`${tab.name[0].toUpperCase()}${tab.name.slice(1)}`}
+          </Nav.Link>
+        </Nav.Item>
+        )
+      })}
     </Nav>
   );
 };
